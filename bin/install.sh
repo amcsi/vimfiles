dbe12797d052a95a5bc390a42211a68908a187d4
Attila Szeremi <attila@szeremi.org>
2016-01-09 00:20:55 +0000

    Install plugins after creating the symlink

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,8 @@
 # Update submodules.
 git submodule update --init --recursive
 
-# Install plugins.
-vim +PluginInstall +qall
-
 # Create symlink from .vim to vimfiles
 ln -s vimfiles ../.vim
+
+# Install plugins.
+vim +PluginInstall +qall
